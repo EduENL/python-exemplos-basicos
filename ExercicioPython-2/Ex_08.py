24568a23f98ff7bcf73f9b8b08034c4daefe0fa1
EduENL <130664287+EduENL@users.noreply.github.com>
2024-08-08 16:17:41 -0300

    .

---
+++ ExercicioPython-2/Ex_08.py
@@ -0,0 +1,11 @@
+#  Exercício 8 - listagem de nomes de alunos 
+print("=====ALUNOS=====")
+print(" ")
+
+# Array das lojas 
+nomes = ["Bruno", "Eduardo", "Isabelle", "Yago", "Marcos", "Pedro"]
+
+# Exibindo Lojas
+for i, nomes in enumerate(nomes,1):
+    print(f"{i} = {nomes}")
+    print(" ")
